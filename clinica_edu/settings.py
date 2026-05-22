6cafe9b89d1d32e483eb2598cab986c0bb3135ca
vinicius Martins Borges <vtorg.martins@gmail.com>
2026-05-22 12:12:51 -0300

    alowed hots '*'

--- clinica_edu/settings.py
+++ clinica_edu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s*49v7#vz=f9#&+en@7+zup+-e3k)&!i+a2)5lw$)vsxk+jhl1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
