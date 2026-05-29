756e4cb30c5da8e552ed7cb4b79ab87c5524cc34
Vinicius Martins <91499371+arruttor@users.noreply.github.com>
2026-05-29 19:50:15 -0300

    Set DEBUG to True for development

--- clinica_edu/settings.py
+++ clinica_edu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s*49v7#vz=f9#&+en@7+zup+-e3k)&!i+a2)5lw$)vsxk+jhl1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://clinicatestversel-production.up.railway.app/"]
