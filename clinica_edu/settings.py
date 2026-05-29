3e43cd782fee7004dd79617595fc347b4036083e
Vinicius Martins <91499371+arruttor@users.noreply.github.com>
2026-05-29 19:38:21 -0300

    Update settings.py

--- clinica_edu/settings.py
+++ clinica_edu/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s*49v7#vz=f9#&+en@7+zup+-e3k)&!i+a2)5lw$)vsxk+jhl1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 
 # Application definition
